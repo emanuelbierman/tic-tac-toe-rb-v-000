951051cf8e50a62545e444b0448fa8abd1dfa95d
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-04-04 20:34:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,6 +110,7 @@ def won?(board)
       if element == "X"
         match << index
       end
+      binding.pry
       WIN_COMBINATIONS.each_with_index do |element, index|
         if (WIN_COMBINATIONS[index]) & match == WIN_COMBINATIONS[index]
           return WIN_COMBINATIONS[index]
